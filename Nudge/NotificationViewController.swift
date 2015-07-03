b062d7a07bb2f055d52635206767df3c4bbdaa07
Antonio <antonioaj@hotmail.co.uk>
2015-07-03 11:35:45 +0100

    Changed Notification Class to BaseController

--- Nudge/NotificationViewController.swift
+++ Nudge/NotificationViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: BaseController {
     @IBOutlet var notificationTable: UITableView!
-
+    
+    var data:[JSON] = []
+    var indexes:[String] = []
+    
+    let cellIdentifier = "ChatListTableViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.notificationTable.registerNib(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
+        self.notificationTable.tableFooterView = UIView(frame: CGRectZero)
+        
+        self.apiRequest(.GET, path: "notifications", closure: { response in
+            
+            println("Notifications url request response ->\(response)");
+            
+            for (id, obj) in response["data"] {
+                self.indexes.append(id)
+                self.data.append(obj)
+            }
+            
+            self.notificationTable!.reloadData()
+        })
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,14 +43,50 @@ class NotificationViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: -- UITableViewDataSource --
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+        
     }
-    */
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.indexes.count
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return 70
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:ChatListTableViewCell = notificationTable.dequeueReusableCellWithIdentifier(cellIdentifier) as! ChatListTableViewCell
+        
+        var title = self.data[indexPath.row]["job"]["title"]
+        cell.jobTitle.text = "re:\(title.stringValue)"
+        
+        return cell
+    }
+    
+    // MARK: -- UITableViewDelegate -
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        var vc:ChatViewController = ChatViewController()
+        
+        //ChatViewController *chatView  = [ChatViewController messagesViewController];
+        //(nibName: "ChatViewController", bundle: nil)
+        
+        vc.chatID = self.data[indexPath.row]["job"]["id"].stringValue;
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
+    
 
 }
