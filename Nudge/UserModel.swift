98c9a15e958407ab40a5b539bf19acb087d0f6d2
Richard Buckle <richardb@sailmaker.co.uk>
2015-11-26 19:56:50 +0000

    #1 Swift 2.0 fixes to UserModel.swift

--- Nudge/UserModel.swift
+++ Nudge/UserModel.swift
@@ -12,6 +12,7 @@ import Alamofire
 import AddressBook
 
 class UserModel: CustomStringConvertible {
+    typealias ErrorHandler = (ErrorType) -> Void
 
     var id: Int?
 
@@ -24,6 +25,7 @@ class UserModel: CustomStringConvertible {
     var status: Int? = 0
 
     var skills:[String]?
+    // TODO: API strings
     var image:[String:String] = ["profile": "http://api.nudj.co/app/placeholder/user.png"]
     var isDefaultImage = true
     var base64Image:String?
@@ -42,45 +44,39 @@ class UserModel: CustomStringConvertible {
     var source:JSON?
     var settings:JSON?
 
-    init() {
-        
-    }
-
-    init(id: Int?, name: String?, token: String?) {
+    init(id: Int? = nil, name: String? = nil, token: String? = nil) {
         self.id = id;
         self.name = name;
         self.token = token;
     }
 
     static func getLocal(closure: ((UserModel?) -> ())) {
+        // TODO: remove singleton access
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
         closure(appDelegate.user)
     }
 
     static func getLocal() -> UserModel? {
+        // TODO: remove singleton access
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
         return appDelegate.user
     }
 
-    static func getCurrent(fields:[String]?, closure: ((UserModel) -> ())? = nil, errorHandler: ((NSError) -> Void)? = nil) {
+    static func getCurrent(fields:[String]?, closure: ((UserModel) -> ())? = nil, errorHandler: ErrorHandler? = nil) {
         UserModel.getById(0, fields: fields, closure: {response in
             let userResponse = response["data"]
 
             let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
-            var user = appDelegate.user == nil ? UserModel() : appDelegate.user!
-
-            let mirror = reflect(user);
-
+            let user = appDelegate.user == nil ? UserModel() : appDelegate.user!
             user.updateFromJson(userResponse)
-
             closure?(user)
-
         }, errorHandler: errorHandler)
     }
+    
     func updateFromJson(source: JSON) {
         self.source = source
         
-        for (key: String, subJson: JSON) in source {
+        for (key, subJson) in source {
             switch key {
             case "id": id = subJson.intValue
             case "status": status = subJson.intValue
@@ -104,80 +100,65 @@ class UserModel: CustomStringConvertible {
             case "settings": settings = subJson
 
             default:
+                // TODO: better error handling
                 print("!!!! Unknown user key: " + key)
             }
         }
     }
 
     func updateImageFromJson(source:JSON) {
-        
-        for (key,val) in source.dictionaryValue {
-            
+        for (key, val) in source.dictionaryValue {
             self.image[key] = val.stringValue;
             self.isDefaultImage = false
-            
         }
         
         if let url = NSURL(string :self.image["profile"]!){
-            var imageData = NSData(contentsOfURL: url)
-                if imageData != nil {
-                    let base64String = imageData!.base64EncodedStringWithOptions([])
-                    self.base64Image = base64String
-                }
+            if let imageData = NSData(contentsOfURL: url) {
+                let base64String = imageData.base64EncodedStringWithOptions([])
+                self.base64Image = base64String
+            }
         }
-        print("updated user image")
-        
     }
 
-    func toggleFavourite(closure: (JSON) -> (), errorHandler: ((NSError) -> Void)? = nil) {
+    func toggleFavourite(closure: (JSON) -> (), errorHandler:ErrorHandler? = nil) {
         if (id == nil) {
             print("Invalid User ID!")
             return;
         }
 
+        // TODO: API strings
         let url = "users/\(id!)/favourite"
-        let method = (favourite == nil || (favourite!) == false) ? Method.PUT : Method.DELETE
+        let method = (favourite ?? false) ? Method.DELETE : Method.PUT;
 
         API.sharedInstance.request(method, path: url, params: nil, closure: closure, errorHandler: errorHandler)
     }
 
-    static func getById(id: Int, fields:[String]?, closure: ((JSON) -> ())? = nil, errorHandler: ((NSError) -> Void)? = nil ) {
+    static func getById(id: Int, fields:[String]?, closure: ((JSON) -> ())? = nil, errorHandler: ErrorHandler? = nil ) {
         let params = [String: AnyObject]()
-
-        let block = closure == nil
-            ? { _ in }
-            : closure
-
-        let userFields = (fields == nil)
-            ? ""
-            : ",".join(fields!)
-
-        let userId = id <= 0
+        let userFields = fields?.reduce("", combine: {$0! + "," + $1}) ?? ""
+        let userId = (id <= 0)
             ? "me"
             : "\(id)"
-
-        
+        // TODO: API strings
         API.sharedInstance.request(Alamofire.Method.GET, path: "users/\(userId)?params=\(userFields)", params: params, closure: closure!, errorHandler: errorHandler)
     }
 
-    static func update(fields: [String: AnyObject], closure: ((JSON) -> ())? = nil, errorHandler: ((NSError) -> Void)? = nil) {
+    static func update(fields: [String: AnyObject], closure: ((JSON) -> ())? = nil, errorHandler: ErrorHandler? = nil) {
         let realClosure = (closure != nil) ? closure! : { _ in }
         let realErrorHandler = (errorHandler != nil) ? errorHandler! : { error in 
             // TODO: handle error
         }
-
+        // TODO: API strings
         API.sharedInstance.put("users/me", params: fields, closure: realClosure, errorHandler: realErrorHandler)
     }
     
 
     static func getImageByContactId(contactId:Int) -> UIImage? {
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
-
         return appDelegate.contacts.getContactImageForId(contactId)
     }
 
     static func getDefaultUserImage() -> UIImage? {
         return UIImage(named: "user_image_placeholder")
     }
-
 }
