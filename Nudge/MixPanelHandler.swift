197c2666396fb2f6a34287bcfa71bb5b08128e65
Richard Buckle <richardb@sailmaker.co.uk>
2015-11-27 01:07:00 +0000

    #2 fix warnings in MixPanel

--- Nudge/MixPanelHandler.swift
+++ Nudge/MixPanelHandler.swift
@@ -10,16 +10,13 @@ import UIKit
 import Mixpanel
 
 class MixPanelHandler: NSObject {
-
-    static func sendData(title:String){
-        
+    static func sendData(title:String) {
         let appdelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let mixpanel = Mixpanel.sharedInstance()
         var username = "unavailable"
         var userId = "unavailable"
         
         if let user = appdelegate.user {
-        
             if user.name != nil {
                 username = user.name!
             }
@@ -27,25 +24,16 @@ class MixPanelHandler: NSObject {
             if user.id != nil{
                 userId = "\(user.id!)"
             }
-            
         }
-        
-        
-        mixpanel.track(title, properties: ["name":username,"id":userId])
+        mixpanel.track(title, properties: ["name":username, "id":userId])
     }
     
-    
     static func startEventTracking(title:String){
-        
-        let appdelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let mixpanel = Mixpanel.sharedInstance()
         mixpanel.timeEvent(title)
-        
     }
     
     static func stopEventTracking(title:String){
-    
-        let appdelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let mixpanel = Mixpanel.sharedInstance()
         mixpanel.track(title)
     }
