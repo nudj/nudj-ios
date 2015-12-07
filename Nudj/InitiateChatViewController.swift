35114a5bbcb09d033b7fb072d69ef9840878c9e7
Richard Buckle <richardb@sailmaker.co.uk>
2015-12-07 19:00:44 +0000

    #21 localise InitiateChatViewController.swift

--- Nudj/InitiateChatViewController.swift
+++ Nudj/InitiateChatViewController.swift
@@ -21,16 +21,11 @@ class InitiateChatViewController: UIViewController, CreatePopupViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "Contact " + self.username!
-        
-
+        self.title = String.localizedStringWithFormat(NSLocalizedString("chat.contact.send.format", comment: ""), self.username!)
     }
-
     
     override func viewWillAppear(animated: Bool) {
-        
         self.textview.becomeFirstResponder()
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -39,7 +34,7 @@ class InitiateChatViewController: UIViewController, CreatePopupViewDelegate {
     }
     
     @IBAction func sendAction(sender: UIBarButtonItem) {
-        
+        // TODO: AP{I strings
         if(!textview.text.isEmpty){
             let params = ["job_id":self.jobid!,"user_id":self.userid!,"notification_id":self.notificationid!,"message":textview.text]
             
@@ -49,30 +44,22 @@ class InitiateChatViewController: UIViewController, CreatePopupViewDelegate {
                 
                 self.textview.resignFirstResponder()
                 self.popup = CreatePopupView(x: 0, yCordinate: 0, width: self.view.frame.size.width , height: self.view.frame.size.height, imageName:"success", withText: true);
-                self.popup!.bodyText("You have successfully contacted \(self.username!)");
+                self.popup!.bodyText(String.localizedStringWithFormat(NSLocalizedString("chat.contact.success.format", comment: ""), self.username!));
                 self.popup?.delegate = self;
                 self.view.addSubview(self.popup!)
 
             }, errorHandler: { error in
                 loggingPrint("error \(error)")
             })
-         
         }
-        
     }
-
     
     @IBAction func cancelAction(sender: UIBarButtonItem) {
-        
         self.dismissViewControllerAnimated(false, completion:nil)
-       
     }
     
     func dismissPopUp() {
         popup!.removeFromSuperview();
         self.dismissViewControllerAnimated(false, completion:nil)
-
     }
-    
-    
 }
