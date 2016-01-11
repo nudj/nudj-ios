ae2e2e7a803630bd1391260ec91a07f393e68af9
Richard Buckle <richardb@sailmaker.co.uk>
2016-01-11 18:23:54 +0000

    #19 Configure Production server for the main app target

---
+++ Nudj/ProductionServer.swift
@@ -0,0 +1,15 @@
+//
+//  ProductionServer.swift
+//  Nudj
+//
+//  Created by Richard Buckle on 11/01/2016.
+//  Copyright © 2016 Nudge I.T. Limited. All rights reserved.
+//
+
+import Foundation
+
+extension API {
+    var server: Server { 
+        return .Production 
+    }
+}
