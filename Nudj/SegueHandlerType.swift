0889e75db8d3e8f1aa2b90413a2fd4e2f761b8b7
Richard Buckle <richardb@sailmaker.co.uk>
2016-01-17 20:03:26 +0000

    Add protocol `SegueHandlerType`
    From WWDC 2015 video 411

---
+++ Nudj/SegueHandlerType.swift
@@ -0,0 +1,29 @@
+//
+//  SegueHandlerType
+//  Nudj
+//
+//  Created by Richard Buckle on 17/01/2016.
+//  Copyright © 2016 Nudge I.T. Limited. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol SegueHandlerType {
+    typealias SegueIdentifier: RawRepresentable
+}
+
+extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
+    func performSegueWithIdentifier(segueIdentifier: SegueIdentifier, sender: AnyObject?) {
+        performSegueWithIdentifier(segueIdentifier.rawValue, sender: sender)
+    }
+    
+    func segueIdentifierForSegue(segue: UIStoryboardSegue) -> SegueIdentifier {
+        guard let identifier = segue.identifier,
+            segueIdentifier = SegueIdentifier(rawValue: identifier)
+            else {
+                fatalError("Invalid segue identifier \(segue.identifier)")
+        }
+        return segueIdentifier
+    }
+}
