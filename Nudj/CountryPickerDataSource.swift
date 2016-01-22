83cd880554ab5afb5d9df7f64808ab8716843a4e
Richard Buckle <richardb@sailmaker.co.uk>
2016-01-22 21:15:34 +0000

    #218 wire up CountryPickerDelegate

--- Nudj/CountryPickerDataSource.swift
+++ Nudj/CountryPickerDataSource.swift
@@ -8,25 +8,37 @@
 
 import UIKit
 
-protocol CountryPickerDelegate: NSObjectProtocol {
+@objc protocol CountryPickerDelegate: class {
     func didSelectData(data: CountryPickerDataSource.Data)
 }
 
 class CountryPickerDataSource: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
-    struct Data {
+    // Ideally Data would be a struct, but that prevents protocol CountryPickerDelegate
+    // from being expressible in Obj-C, which prevents the delegate from being an IBOutlet.
+    // This is a compromise solution biased towards wiring the delagate in IB rather than in code.
+    // Not as Swifty as I'd like, but no worse than a standard Obj-C implementation would be.
+    class Data: NSObject { 
         let country: String
         let diallingCode: String
         let iso2Code: String
+        
+        init(country: String = "", diallingCode: String = "", iso2Code: String = "") {
+            self.country = country
+            self.diallingCode = diallingCode
+            self.iso2Code = iso2Code
+            super.init()
+        }
     }
     
     let data: [Data]
-    weak var delegate: CountryPickerDelegate?
+    @IBOutlet weak var delegate: CountryPickerDelegate?
     
     override convenience init() {
+        // init from static data "Dialling Codes.plist" in the main bundle
         if let url = NSBundle.mainBundle().URLForResource("Dialling Codes", withExtension: "plist"),
             root = NSDictionary(contentsOfURL: url),
             countries = root["countries"] as? [[String:String]] {
-            let data: [Data] = countries.map{Data(country: $0["country"]!, diallingCode: $0["dialling code"]!, iso2Code: $0["iso2"]!)}
+            let data: [Data] = countries.map{Data(country: $0["country"]!, diallingCode: $0["diallingCode"]!, iso2Code: $0["iso2Code"]!)}
             self.init(data: data)
         } else {
             self.init(data: [])
