b714718b14a0400a0db500b4397fc6a11b319883
Richard Buckle <richardb@sailmaker.co.uk>
2016-02-15 14:09:45 +0000

    #19 Add `API+Endpoints.swift`

---
+++ Nudj/API+Endpoints.swift
@@ -0,0 +1,15 @@
+//
+//  API+Endpoints.swift
+//  Nudj
+//
+//  Created by Richard Buckle on 15/02/2016.
+//  Copyright © 2016 Nudge I.T. Limited. All rights reserved.
+//
+
+import Foundation
+
+extension API {
+    struct Endpoints {
+        //
+    }
+}
