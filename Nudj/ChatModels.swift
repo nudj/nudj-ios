30518f98e275f337a2ff29c5c64069d09ac843b8
Richard Buckle <richardb@sailmaker.co.uk>
2016-03-10 17:04:27 +0000

    Disable TLS for chat for now
    Pending resolution of https://github.com/Nudj/nudj-backend/issues/12

--- Nudj/ChatModels.swift
+++ Nudj/ChatModels.swift
@@ -60,7 +60,6 @@ class ChatModels: NSObject, XMPPStreamDelegate, XMPPRosterDelegate, XMPPRoomDele
         xmppStream = XMPPStream();
         let api = API()
         xmppStream?.hostName = api.server.chatHostname
-        xmppStream?.autoStartTLS = true
         
         xmppReconnect = XMPPReconnect();
         xmppRosterStorage = XMPPRosterCoreDataStorage();
