bec7ab7b67304630be0e9767af826f1e970d44f3
Richard Buckle <richardb@sailmaker.co.uk>
2016-05-17 18:29:07 +0100

    #127 implement job URL builder with tests

--- Nudj/JobURL.swift
+++ Nudj/JobURL.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum JobURL {
+    case Main(Int), Preview(Int)
+    
+    func path() -> String {
+        switch self {
+        case .Main(let jobID):
+            return "/job/\(jobID)"
+        case .Preview(let jobID):
+            return "/jobpreview/\(jobID)"
+        }
+    }
+    
+    func url() -> NSURL {
+        let api = API()
+        let components = NSURLComponents()
+        components.scheme = "https"
+        components.host = api.server.mobileWebHostname
+        components.path = self.path()
+        let url = components.URL!
+        return url
+    }
+}
--- NudjTests/JobURLTests.swift
+++ NudjTests/JobURLTests.swift
@@ -8,3 +8,27 @@
 import Foundation
 import XCTest
 @testable import Nudj
+
+class JobURLTests: XCTestCase {
+    var hostname: String = ""
+    
+    override func setUp() {
+        super.setUp()
+        let api = API()
+        hostname = api.server.mobileWebHostname
+    }
+    
+    func testMainURL() {
+        let jobURL: JobURL = .Main(42)
+        let url = jobURL.url()
+        let expectedURL = NSURL(string: "https://\(hostname)/job/42")!
+        XCTAssertEqual(url, expectedURL)
+    }
+    
+    func testPreviewURL() {
+        let jobURL: JobURL = .Preview(42)
+        let url = jobURL.url()
+        let expectedURL = NSURL(string: "https://\(hostname)/jobpreview/42")!
+        XCTAssertEqual(url, expectedURL)
+    }
+}
