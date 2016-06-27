23b35b3e7cae784731e5c22b446bd619eeb2885b
Richard Buckle <richardb@sailmaker.co.uk>
2016-06-28 00:09:37 +0100

    Add `JobModelTests.swift`

---
+++ NudjTests/JobModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  JobModelTests.swift
+//  Nudj
+//
+//  Copyright © 2016 Nudge I.T. Limited. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import Nudj
+
+class JobModelTests: XCTestCase {
+    var locale: NSLocale = NSLocale.systemLocale()
+    
+    func test() {
+        //
+    }
+}
