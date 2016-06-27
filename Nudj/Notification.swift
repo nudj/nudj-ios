084d2b91842d8306a32ac15c5f18074e2ee9f0dc
Richard Buckle <richardb@sailmaker.co.uk>
2016-06-27 19:25:47 +0100

    #42 Use the JobModel to format bonuses with currency
    Also straightened out needless optionality in the notification properties.

--- Nudj/Notification.swift
+++ Nudj/Notification.swift
@@ -1,3 +1,4 @@
+
 //
 //  Notification.swift
 //  Nudj
@@ -19,37 +20,39 @@ enum NotificationType:Int {
 }
 
 class Notification {
-    var notificationId:String?
-    var notificationType:NotificationType?
-    var notificationReadStatus:Bool?
-    var notificationTime:Double?
-    var notificationMessage:String?
+    var notificationId: String
+    var notificationType: NotificationType
+    var notificationReadStatus: Bool
+    var notificationTime: Double
+    var notificationMessage: String
     
-    var jobID:String?
-    var jobMessage:String?
-    var jobTitle:String?
-    var jobBonus:String?
+    var jobID: Int
+    var jobMessage: String
+    var jobTitle: String
+    var jobBonus: String
     
-    var employerName:String?
+    var employerName: String
     
-    var senderId:String?
-    var senderName:String?
-    var senderImage:String?
-    var senderPhoneNumber:String?
+    var senderId: Int
+    var senderName: String
+    var senderImage: String
+    var senderPhoneNumber: String
     
-    var chatId:String?
+    var chatId: Int
     
     init?(json: JSON) {
         guard let rawType = Int(json["type"].stringValue) else {
             return nil
         }
-        let type = NotificationType(rawValue: rawType)
+        guard let type = NotificationType(rawValue: rawType) else {
+            return nil
+        }
 
         // TODO: API strings
         let sender = json["sender"]
         let metadata = json["meta"]
         senderImage = sender["image"]["profile"].stringValue
-        senderId = sender["id"].stringValue
+        senderId = sender["id"].intValue
         senderName = sender["name"].stringValue
         senderPhoneNumber = metadata["phone"].stringValue
         
@@ -57,8 +60,14 @@ class Notification {
         
         jobBonus = metadata["job_bonus"].stringValue
         jobMessage = metadata["message"].stringValue
-        jobID = metadata["job_id"].stringValue
+        jobID = metadata["job_id"].intValue
         jobTitle = metadata["job_title"].stringValue
+        
+        let bonusAmount = metadata["job_bonus"].intValue
+        let bonusCurrency = metadata["job_currency"].string ?? ""
+        
+        let job = JobModel(title: jobTitle, description: "", salaryFreeText: "", company: employerName, location: "", bonusAmount: bonusAmount, bonusCurrency: bonusCurrency, active: true, skills: [])
+        jobBonus = job.formattedBonus 
     
         notificationReadStatus = json["read"].boolValue
         notificationTime = json["created"].doubleValue
@@ -67,6 +76,6 @@ class Notification {
         
         notificationType = type
         
-        chatId =  metadata["chat_id"].stringValue
+        chatId =  metadata["chat_id"].intValue
     }
 }
