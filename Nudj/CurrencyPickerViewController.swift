93d68e9db90a4b9b4409d67a9842c8a7b2bf8c83
Richard Buckle <richardb@sailmaker.co.uk>
2016-07-05 17:20:50 +0100

    #42 Add currency picker view controller

---
+++ Nudj/CurrencyPickerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyPickerViewController.swift
+//  Nudj
+//
+//  Created by Richard Buckle on 05/07/2016.
+//  Copyright © 2016 Nudge I.T. Limited. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyPickerViewController: UIViewController {
+    
+}
