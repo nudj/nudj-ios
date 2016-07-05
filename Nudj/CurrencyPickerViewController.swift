78c8e0675714ed366b4054ee08fcc9685bcec783
Richard Buckle <richardb@sailmaker.co.uk>
2016-07-05 18:51:20 +0100

    #42 Implement CurrencyPickerViewController

--- Nudj/CurrencyPickerViewController.swift
+++ Nudj/CurrencyPickerViewController.swift
@@ -8,6 +8,49 @@
 
 import UIKit
 
+protocol CurrencyPickerDelegate: class {
+    func didSelectCurrency(isoCode: String, symbol: String)
+}
+
 class CurrencyPickerViewController: UIViewController {
+    @IBOutlet weak var currencyTable: UITableView!
+    @IBOutlet var dataSource: CurrencyPickerDataSource!
     
+    weak var delegate: CurrencyPickerDelegate?
+    
+    var selectedCurrencyIsoCode: String? {
+        get {
+            guard let indexPath = currencyTable.indexPathForSelectedRow else {
+                return nil
+            }
+            let data = dataSource.dataForIndexPath(indexPath)
+            return data?.isoCode
+        }
+        set {
+            let isoCode = newValue ?? dataSource.nativeCurrency
+            if let indexPath = dataSource.indexPathForCurrencyCode(isoCode) {
+                currencyTable.selectRowAtIndexPath(indexPath, animated: true, scrollPosition: UITableViewScrollPosition.None)
+            } else {
+                if let indexPath = currencyTable.indexPathForSelectedRow {
+                    currencyTable.deselectRowAtIndexPath(indexPath, animated: true)
+                }
+            }
+        }
+    }
+    
+    func symbolForCurrencyCode(isoCode: String) -> String {
+        return dataSource.symbolForCurrency(isoCode)
+    }
+    
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done(sender: AnyObject) {
+        if let isoCode = selectedCurrencyIsoCode {
+            let symbol = symbolForCurrencyCode(isoCode)
+            delegate?.didSelectCurrency(isoCode, symbol: symbol)
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
